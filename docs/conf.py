597f8796e5b2581627f6f5cd61032833c8b882a6
RaphaelHebert <raphaelhebert18@gmail.com>
2021-10-06 15:49:09 +0700

    changed title level in docs/main.md

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Raphael Hebert'
 author = 'Raphael Hebert'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.16'
+release = '0.1.17'
 
 
 # -- General configuration ---------------------------------------------------
@@ -56,3 +56,18 @@ html_static_path = ['_static']
 
 # Allow usage of markdown format
 extensions = ['myst_parser']
+
+html_theme_options = {
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'yellow',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
--- docs/main.md
+++ docs/main.md
@@ -3,6 +3,7 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 [![pypi](https://img.shields.io/pypi/v/nsdpy)](https://pypi.org/project/nsdpy/)
 [![Python 3.8](https://upload.wikimedia.org/wikipedia/commons/a/a5/Blue_Python_3.8_Shield_Badge.svg)](https://www.python.org/)  
+[![Documentation Status](https://readthedocs.org/projects/nsdpy/badge/?version=latest)](https://nsdpy.readthedocs.io/en/latest/?badge=latest)
 
 # Overview  
 
