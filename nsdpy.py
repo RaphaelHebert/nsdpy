268725edeab6a487d094ece0e40e01f297f800b4
Joe Hebert <raphaelhebert18@gmail.com>
2022-01-30 10:32:19 +0100

    added tsv_file_writer() and refactored fasta()

--- nsdpy.py
+++ nsdpy.py
@@ -64,6 +64,9 @@ def main():
         classif = 3
 
     OPTIONS = (verb, args.cds, classif, args.taxids, args.tsv, args.information)
+
+    print(OPTIONS)      #for testing only
+
     QUERY = (args.request, args.apikey)
 
     ##foldername and path
