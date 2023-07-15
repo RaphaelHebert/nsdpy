79b25f5fac97129229eacc7b92724558a003e35b
raphael-corize <raphael.hebert@corize.co.jp>
2023-07-15 11:23:08 +0200

    added test case for search UTs

--- tests/test_search.py
+++ tests/test_search.py
@@ -5,7 +5,7 @@ from operator import attrgetter
 from functions import search
 from data import search_data
  
-dna_1, seq_1, response_1 = attrgetter("dna_1", "seq_1", "response_1")(search_data)
+dna_1, seq_1, response_1, response_2 = attrgetter("dna_1", "seq_1", "response_1", "response_2")(search_data)
 
 
 class testsFunctions(unittest.TestCase):
@@ -14,6 +14,9 @@ class testsFunctions(unittest.TestCase):
         # should parse data correctly
         parse_result = search(dna_1 ,{}, seq_1)
         self.assertDictEqual(parse_result, response_1)
-        
+
+        #should return "not found"
+        parse_result = search('someRandomString12434#@!$jnefsdf' ,{}, 'someRandomString12434#@!$jnefsdf')
+        self.assertDictEqual(parse_result, response_2)
 if __name__=='__main__':
     unittest.main()
