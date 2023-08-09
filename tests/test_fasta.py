88fb151504cc95182b3c1ffa729992b81381f87c
RaphaelHebert <raphaelhebert18@gmail.com>
2023-08-09 21:38:03 +0700

    added tests for created files by fasta function

--- tests/test_fasta.py
+++ tests/test_fasta.py
@@ -1,6 +1,9 @@
 import unittest
 from unittest.mock import patch
 from operator import attrgetter
+import filecmp
+import os
+import shutil
 
 # local import
 from functions import fasta
@@ -13,6 +16,7 @@ expected_normal_dict_id = attrgetter("fasta_expected_result_dict_id")(fasta_data
 ## TODO:
 # test for output file -t option (./tsv/filename.tsv)
 # test for fasta output file (./fasta/filename.fasta with -t and /filename.fasta without)
+
 class testsFunctions(unittest.TestCase):
     @patch("functions.download")
     def test_fasta(self, get_content_mock):
@@ -31,13 +35,25 @@ class testsFunctions(unittest.TestCase):
         get_content_mock.return_value = Response()
 
         # should return expected result
-        fasta_result = fasta(path, dict_ids, dict_taxo, QUERY, list_of_ids)
+        OPTIONS = (None, None, None, None, True, None)
+
+        fasta_result = fasta(path, dict_ids, dict_taxo, QUERY, list_of_ids, OPTIONS)
+        # function output
         self.assertEqual(fasta_result, expected_normal_result)
+        # files
+        self.assertTrue(filecmp.cmp("./tests/data/fasta_expected.fasta", "./fasta/others.fasta"))
+        self.assertTrue(filecmp.cmp("./tests/data/fasta_expected.tsv", "./tsv/others.tsv"))
 
         # # should return an empty dict
         # parse_result = parseClassifXML("some random strings")
         # self.assertEqual(parse_result, {})
 
+    def test_fasta_cleanup(self):
+        # make sure the created file in test_taxids is cleaned up even on test fail
+        if os.path.exists("fasta/"):
+            shutil.rmtree("fasta/")
+        if os.path.exists("tsv/"):
+            shutil.rmtree("tsv/")
 
 if __name__ == "__main__":
     unittest.main()
