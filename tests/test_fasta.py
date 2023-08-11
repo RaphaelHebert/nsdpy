90a1f04c66f2afb359e398ab75205ba39140cdb2
raphael-corize <raphael.hebert@corize.co.jp>
2023-08-11 19:15:34 +0200

    added test for verbose == 1

--- tests/test_fasta.py
+++ tests/test_fasta.py
@@ -3,6 +3,7 @@ from unittest.mock import patch
 from operator import attrgetter
 import filecmp
 import os
+import io
 import shutil
 
 # local import
@@ -14,27 +15,41 @@ expected_normal_result = attrgetter("fasta_expected_returned_result")(fasta_data
 expected_normal_dict_id = attrgetter("fasta_expected_result_dict_id")(fasta_data)
 
 
+def cleanup(path):
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        if os.path.exists("tsv/"):
+            shutil.rmtree("tsv/")
+
+
+# mock
+class Response:
+    text = expected_download_normal_result
+
+
 class testsFunctions(unittest.TestCase):
     @patch("functions.download")
     def test_fasta(self, get_content_mock):
-        # arguments
-        path = "."
-        dict_ids = expected_normal_dict_id
-        dict_taxo = {}
-        QUERY = ("mocked_query", "mocked_key")
-        OPTIONS = (None, None, None, None, True, None)  # -t option
-        list_of_ids = ["mocked_id_one", "mocked_id_two", "mocked_id_three"]
-
-        # split in parts and passed to the mocked dl function
-
-        # mock
-        class Response:
-            text = expected_download_normal_result
+        arguments = {
+            "path": ".",
+            "dict_ids": expected_normal_dict_id,
+            "dict_taxo": {},
+            "QUERY": ("mocked_query", "mocked_key"),
+            "OPTIONS": (None, None, None, None, True, None),  # -t option
+            "list_of_ids": ["mocked_id_one", "mocked_id_two", "mocked_id_three"],
+        }
 
         get_content_mock.return_value = Response()
 
         # should return expected result
-        fasta_result = fasta(path, dict_ids, dict_taxo, QUERY, list_of_ids, OPTIONS)
+        fasta_result = fasta(
+            arguments["path"],
+            arguments["dict_ids"],
+            arguments["dict_taxo"],
+            arguments["QUERY"],
+            arguments["list_of_ids"],
+            arguments["OPTIONS"],
+        )
         # function output
         self.assertEqual(fasta_result, expected_normal_result)
         # files
@@ -44,12 +59,37 @@ class testsFunctions(unittest.TestCase):
         self.assertTrue(
             filecmp.cmp("./tests/data/fasta_expected.tsv", "./tsv/OTHERS.tsv")
         )
+        cleanup("tsv/")
+        cleanup("fasta/")
 
-    def test_fasta_cleanup(self):
-        if os.path.exists("fasta/"):
-            shutil.rmtree("fasta/")
-        if os.path.exists("tsv/"):
-            shutil.rmtree("tsv/")
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_fasta_stdout(self, mock_stdout):
+
+        arguments = {
+            "path": ".",
+            "dict_ids": expected_normal_dict_id,
+            "dict_taxo": {},
+            "QUERY": ("mocked_query", "mocked_key"),
+            "OPTIONS": (1, None, None, None, True, None),  # -t option
+            "list_of_ids": ["mocked_id_one", "mocked_id_two", "mocked_id_three"],
+        }
+
+        with patch("functions.download") as mocked_get:
+            mocked_get.side_effect = [Response()]
+            fasta(
+                arguments["path"],
+                arguments["dict_ids"],
+                arguments["dict_taxo"],
+                arguments["QUERY"],
+                arguments["list_of_ids"],
+                arguments["OPTIONS"],
+            )
+            self.assertEqual(mock_stdout.getvalue(), "Downloading the fasta files...\n")
+
+        # self.assertEqual(mock_stdout.getvalue(), "Downloading the fasta files...")
+        # clear mock_stdout value
+        mock_stdout.seek(0)
+        mock_stdout.truncate(0)
 
 
 if __name__ == "__main__":
