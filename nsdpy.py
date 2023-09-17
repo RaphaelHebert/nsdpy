30379a03c9e13fdd35d27de20d02ed8c9a1ddd0a
raphael-corize <raphael.hebert@corize.co.jp>
2023-09-17 22:11:52 +0200

    updated pipeline for complete semver  match only test fail

--- nsdpy.py
+++ nsdpy.py
@@ -1,4 +1,4 @@
-__version__ = "v0.3.23-beta"
+__version__ = "0.3.23-beta"
 __author__ = "Raphael Hebert, Emese Meglecz"
 __email__ = "raphaelhebert18@gmail.com, emese.meglecz@imbe.fr"
 __license__ = "MIT"
