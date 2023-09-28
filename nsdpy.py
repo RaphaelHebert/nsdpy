482e339a9c173a3778a3f3792126ddc46a6c6261
raphael-corize <raphael.hebert@corize.co.jp>
2023-09-28 22:13:23 +0200

    updated certifi and bump to 0.3.31-beta

--- nsdpy.py
+++ nsdpy.py
@@ -1,4 +1,4 @@
-__version__ = "0.3.31-beta"
+__version__ = "0.3.32-beta"
 __author__ = "Raphael Hebert, Emese Meglecz"
 __email__ = "raphaelhebert18@gmail.com, emese.meglecz@imbe.fr"
 __license__ = "MIT"
